0d829348be303a1c7527519e56b23de02d427ac8
Hak-Vichet <hak.vichet19@kit.edu.kh>
2024-02-02 16:38:19 +0700

    update state

--- src/components/LightBoxImages.vue
+++ src/components/LightBoxImages.vue
@@ -1,12 +1,11 @@
-<!-- ProductGallery.vue -->
-
 <template>
   <div class="mt-10 grid md:grid-cols-2 p-10">
     <div class="p-5">
       <img
-        :src="selectedImage"
+        :src="selectedImage.src"
         alt="Product"
         class="cursor-pointer object-cover rounded-[10px]"
+        @click="openLightbox"
       />
 
       <div class="flex mt-5">
@@ -28,57 +27,85 @@
       <div class="relative">
         <button
           @click="closeLightbox"
-          class="absolute top-2 right-2 text-white text-xl cursor-pointer"
+          class="absolute top-2 right-2 text-white text-xl cursor-pointer z-10"
         >
           &times;
         </button>
 
-        <img @click="selectImage(selectedImage.id)" alt="Product" class="w-96 h-96 object-cover" />
+        <img :src="selectedImage.src" alt="Product" class="w-96 h-96 object-cover" />
       </div>
     </div>
 
     <div class="flex flex-col gap-y-3 my-auto p-5">
       <p class="text-[12px] font-bold text-amber-500">SNEAKER COMPANY</p>
       <h1 class="text-3xl font-bold">Fall Limited Edition Sneakers</h1>
-      <p class="text-[10px] text-gray-400">
-        These low -profile sneakers are your perfect casual wear companion. Featuring a durable
-        rubber outer sole, they'll wishstand everything the weather can offer.
+      <p class="text-[10px] text-gray-400 mt-2">
+        These low-profile sneakers are your perfect casual wear companion. Featuring a durable
+        rubber outer sole, they'll withstand everything the weather can offer.
       </p>
-    </div>
 
-    <!-- Lightbox -->
+      <div class="flex gap-x-3 items-center mt-2">
+        <div class="text-2xl font-bold">{{ totalPrice.finalPrice }}.00</div>
+        <div class="px-2 bg-orange-100 rounded-md text-[13px] text-orange-500">
+          {{ selectedImage.discount }}
+        </div>
+      </div>
+
+      <div class="line-through text-[12px] text-gray-400 mt-[-10px]">
+        {{ selectedImage.price }}.00
+      </div>
+
+      <OrderProduct @decrese-price="calculatePrice1" @increase-price="calculatePrice2" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, reactive, type Ref } from 'vue';
 
-const lightBoxOpen = ref(false)
+import OrderProduct from "@/components/OrderProduct.vue"
 
-const thumbnails = ref([
-  { id: 1, src: '/image-product-1.jpg', price: '$200', discount: '20%', finalPrice: '$160' },
-  { id: 2, src: '/image-product-2.jpg', price: '$300', discount: '30%', finalPrice: '$260' },
-  // { id: 3, src: '/image-product-3.jpg', price: '$400', discount: '10%', finalPrice: '$360' },
-  // { id: 3, src: '/image-product-4.jpg', price: '$500', discount: '5%', finalPrice: '$460' }
-])
+interface Thumbnail {
+  id: number;
+  src: string;
+  price: string;
+  discount: string;
+  finalPrice: number;
+}
 
-const selectedImage = ref('/image-product-1.jpg')
-console.log('Selected Image: ', selectedImage.value)
+const lightBoxOpen = ref(false);
+
+const thumbnails = ref<Thumbnail[]>([
+  { id: 1, src: '/image-product-1.jpg', price: '$200', discount: '20%', finalPrice: 160 },
+  { id: 2, src: '/image-product-2.jpg', price: '$300', discount: '30%', finalPrice: 360 },
+  { id: 3, src: '/image-product-3.jpg', price: '$400', discount: '10%', finalPrice: 360 },
+  { id: 4, src: '/image-product-4.jpg', price: '$500', discount: '5%', finalPrice: 460 }
+]);
+
+const selectedImage: Ref<Thumbnail> = ref(thumbnails.value[0]);
+const selectedImagePrice: Ref<number> = ref(selectedImage.value.finalPrice);
+const totalPrice = reactive({
+  finalPrice: selectedImagePrice.value
+});
+
+const calculatePrice1 = (number: number) => {
+  totalPrice.finalPrice = number * selectedImage.value.finalPrice;
+};
+
+const calculatePrice2 = (number: number) => {
+  totalPrice.finalPrice = number * selectedImage.value.finalPrice;
+};
 
 const selectImage = (id: number) => {
-  const showImageId = thumbnails.value[id]
-
-  const imageToShow = showImageId.src
-
-  selectedImage.value = imageToShow
-
-  console.log('Selected Image: ', showImageId)
-  console.log('Image to show: ', imageToShow)
-  // selectedImage.value = showImage.src
-  // console.log('Selected index: ', thumbnails.value[id])
-}
+  selectedImage.value = thumbnails.value.find((thumbnail) => thumbnail.id === id) || thumbnails.value[0];
+};
 
 const closeLightbox = () => {
-  lightBoxOpen.value = false
-}
+  lightBoxOpen.value = false;
+};
+
+const openLightbox = () => {
+  lightBoxOpen.value = true;
+};
+
 </script>
---
+++ src/components/OrderProduct.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="flex gap-x-4 mt-5">
+        <div class="flex h-[30px] items-center">
+            <button @click="decreseItemAndPrice"  class="px-5 py-1 text-orange-500 bg-gray-200">-</button>
+            <p class="px-5 py-1 bg-gray-200">{{ number }}</p>
+            <button @click="increseItemAndPrice" class="px-5 py-1 text-orange-500 bg-gray-200">+</button>
+        </div>
+
+        <button class="flex flex-1 py-1 px-auto bg-orange-500 rounded-sm">
+            <p class=" text-orange-200 text-[15px] mx-auto">Add to card</p>
+        </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import { ref, defineEmits } from 'vue'
+
+const number = ref(1)
+
+const emit = defineEmits(['increasePrice', 'decresePrice'])
+
+const increseItemAndPrice = () => {
+    number.value += 1
+    emit('increasePrice', number.value)
+}
+
+const decreseItemAndPrice = () => {
+    if(number.value > 1) {
+        number.value -= 1
+        emit('decresePrice', number.value);
+    }
+}
+
+
+</script>
