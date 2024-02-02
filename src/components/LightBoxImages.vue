34e4d5c6dc70f5e7bf67c22def55c14c24361ff9
Hak-Vihet <hak.vichet19@kit.edu.kh>
2024-02-02 13:13:49 +0700

    fixing image display

--- src/components/LightBoxImages.vue
+++ src/components/LightBoxImages.vue
@@ -3,30 +3,39 @@
 <template>
   <div class="mt-10 grid md:grid-cols-2 p-10">
     <div class="p-5">
-      <img :src="selectedImage.src" alt="Product" class="cursor-pointer object-cover rounded-[10px]" />
+      <img
+        :src="selectedImage"
+        alt="Product"
+        class="cursor-pointer object-cover rounded-[10px]"
+      />
 
       <div class="flex mt-5">
         <div v-for="thumbnail in thumbnails" :key="thumbnail.id" class="flex">
           <img
-          :src="thumbnail.src"
+            :src="thumbnail.src"
             alt="thumbnail"
             class="flex w-[80px] h-[80px] rounded-[10px] mr-10"
+            @click="selectImage(thumbnail.id)"
           />
         </div>
       </div>
     </div>
 
-    <!-- <div
+    <div
+      v-if="lightBoxOpen"
       class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 flex items-center justify-center"
     >
       <div class="relative">
+        <button
+          @click="closeLightbox"
+          class="absolute top-2 right-2 text-white text-xl cursor-pointer"
+        >
+          &times;
+        </button>
 
-        <button class="absolute top-2 right-2 text-white text-xl cursor-pointer">&times;</button>
-
-
-        <img alt="Product" class="w-96 h-96 object-cover" />
+        <img @click="selectImage(selectedImage.id)" alt="Product" class="w-96 h-96 object-cover" />
       </div>
-    </div> -->
+    </div>
 
     <div class="flex flex-col gap-y-3 my-auto p-5">
       <p class="text-[12px] font-bold text-amber-500">SNEAKER COMPANY</p>
@@ -49,12 +58,27 @@ const lightBoxOpen = ref(false)
 const thumbnails = ref([
   { id: 1, src: '/image-product-1.jpg', price: '$200', discount: '20%', finalPrice: '$160' },
   { id: 2, src: '/image-product-2.jpg', price: '$300', discount: '30%', finalPrice: '$260' },
-  { id: 3, src: '/image-product-3.jpg', price: '$400', discount: '10%', finalPrice: '$360' },
-  { id: 3, src: '/image-product-4.jpg', price: '$500', discount: '5%', finalPrice: '$460' }
+  // { id: 3, src: '/image-product-3.jpg', price: '$400', discount: '10%', finalPrice: '$360' },
+  // { id: 3, src: '/image-product-4.jpg', price: '$500', discount: '5%', finalPrice: '$460' }
 ])
 
-const selectedImage = ref(thumbnails.value[0])
+const selectedImage = ref('/image-product-1.jpg')
+console.log('Selected Image: ', selectedImage.value)
 
+const selectImage = (id: number) => {
+  const showImageId = thumbnails.value[id]
 
+  const imageToShow = showImageId.src
 
+  selectedImage.value = imageToShow
+
+  console.log('Selected Image: ', showImageId)
+  console.log('Image to show: ', imageToShow)
+  // selectedImage.value = showImage.src
+  // console.log('Selected index: ', thumbnails.value[id])
+}
+
+const closeLightbox = () => {
+  lightBoxOpen.value = false
+}
 </script>
