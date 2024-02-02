04db870bc9ae41f0dd2d17f15ade36067a893484
Hak-Vihet <hak.vichet19@kit.edu.kh>
2024-02-02 08:48:44 +0700

    update routes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <header>
-    <div>hello</div>
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -17,7 +11,18 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </div> -->
     <!-- <HelloWorld msg="You did it!" /> -->
+
+    <NavBar />
+    
   </header>
 
   <RouterView />
 </template>
+
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+import NavBar from '@/components/NavBar.vue'
+
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex items-center">
+    <div class="p-3 text-2xl font-bold">sneakers</div>
+        <router-link to="/" class="p-3 ml-4 text-gray-500">Collections</router-link>
+        <router-link to="/men" class="p-3 ml-4 text-gray-500">Men</router-link>
+        <router-link to="/women" class="p-3 ml-4 text-gray-500">Women</router-link>
+        <router-link to="/about" class="p-3 ml-4 text-gray-500">About</router-link>
+        <router-link to="/contact" class="p-3 ml-4 text-gray-500">Contact</router-link>
+
+    <div>
+        
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink} from 'vue-router'
+
+</script>
