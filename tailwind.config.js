597d7c9a6ef7d5d0ff104c9be896360b07482037
Hak-Vichet <hak.vichet19@kit.edu.kh>
2024-02-03 10:48:22 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    screens: {
-      sm: '400px',
-      md: '600px', // Set it to a smaller value than the default 768px
-      lg: '1024px',
-      xl: '1280px'
-    },
-    plugins: []
-  }
+    extend: {}
+  },
+  plugins: []
 }
